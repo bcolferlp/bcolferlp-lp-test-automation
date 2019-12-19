56d0a03a0a45f135bec1b507d822ca26c581e4ef
ngonzalez625 <ngonzalez@loanpal.com>
2019-12-19 14:16:16 -0800

    fix: refactored loan creation, results and files

--- tests/loanDocsDownload.test.js
+++ tests/loanDocsDownload.test.js
@@ -1,33 +1,31 @@
-import each from 'jest-each'
-const LoanDocsResultsFiles = require('../src/utilities/loanDocsResultFiles')
+import each from 'jest-each';
 
+const LoanDocsResultsFiles = require('../src/utilities/loanDocsResultFiles');
 
 describe('Download DocuSign PDF files', () => {
+  const loanResults = new LoanDocsResultsFiles();
+  const loanSingleBorrSunRun = loanResults.getLoanSingleBorrSunRun();
+  const loanSingleBorrNonSunRun = loanResults.getLoanSingleBorrNonSunRun();
+  const loanCoBorrSunRun = loanResults.getLoanCoBorrSunRun();
+  const loanCoBorrNonSunRun = loanResults.getLoanCoBorrNonSunRun();
 
-    const loanResults = new LoanDocsResultsFiles()
-    const loanSingleBorrSunRun = loanResults.getLoanSingleBorrSunRun()
-    const loanSingleBorrNonSunRun = loanResults.getLoanSingleBorrNonSunRun()
-    const loanCoBorrSunRun = loanResults.getLoanCoBorrSunRun()
-    const loanCoBorrNonSunRun = loanResults.getLoanCoBorrNonSunRun()
+  each(loanSingleBorrSunRun).test('Download Single Borrower SunRun Loans', ({ loanId, firstName }) => {
+    console.log(loanId);
+    console.log(firstName);
+  });
 
-    each(loanSingleBorrSunRun).test('Download Single Borrower SunRun Loans', (loanId, firstName) => {
-        console.log(loanId)
-        console.log(firstName)
-    })
+  each(loanSingleBorrNonSunRun).test('Download Single Borrower Non SunRun Loans', ({ loanId, firstName }) => {
+    console.log(loanId);
+    console.log(firstName);
+  });
 
-    each(loanSingleBorrNonSunRun).test('Download Single Borrower Non SunRun Loans', (loanId, firstName) => {
-        console.log(loanId)
-        console.log(firstName)
-    })
+  each(loanCoBorrSunRun).test('Download Combined SunRun Loans', ({ loanId, firstName }) => {
+    console.log(loanId);
+    console.log(firstName);
+  });
 
-    each(loanCoBorrSunRun).test('Download Combined SunRun Loans', (loanId, firstName) => {
-        console.log(loanId)
-        console.log(firstName)
-    })
-
-    each(loanCoBorrNonSunRun).test('Download Combined Non SunRun Loans', (loanId, firstName) => {
-        console.log(loanId)
-        console.log(firstName)
-    })
-
-})
+  each(loanCoBorrNonSunRun).test('Download Combined Non SunRun Loans', ({ loanId, firstName }) => {
+    console.log(loanId);
+    console.log(firstName);
+  });
+});
