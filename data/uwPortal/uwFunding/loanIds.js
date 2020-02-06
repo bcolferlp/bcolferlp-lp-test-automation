6b407cb40e5b2f3afd7f0401ee8b1eeb2d16ce0a
ngonzalez625 <ngonzalez@loanpal.com>
2020-02-06 15:02:28 -0800

    chore: migrated funding data for dev

---
+++ data/uwPortal/uwFunding/loanIds.js
@@ -0,0 +1,7 @@
+const validSolarLoanIds = ['19-16-005924', '19-16-008156', '19-16-009553'];
+const readyToFundAfter4pmLoanIds = ['19-13-009190', '19-02-009224', '19-14-007467'];
+
+module.exports = {
+  validSolarLoanIds,
+  readyToFundAfter4pmLoanIds
+};
