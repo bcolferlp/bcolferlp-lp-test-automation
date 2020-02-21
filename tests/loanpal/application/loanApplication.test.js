a02761a2f93cb655ac60d42e3aa7c61b7f6db515
ngonzalez625 <ngonzalez@loanpal.com>
2020-02-20 17:19:45 -0800

    wip: loan creation with UW and PP validate

--- tests/loanpal/application/loanApplication.test.js
+++ tests/loanpal/application/loanApplication.test.js
@@ -5,67 +5,98 @@ import LPAppPage from '../../../src/pages/loanpal/application/lpAppPage';
 import ClientData from '../../../src/utilities/clientData';
 import LoanData from '../../../src/utilities/loanData';
 import ElasticClient from '../../../src/utilities/elasticClient';
+import UWLoginPage from '../../../src/pages/uwPortal/uwLogin/uwLoginPage';
+import UWLoanDetailsPage from '../../../src/pages/uwPortal/uwLoanDetails/uwLoanDetailsPage';
+import PPLoginPage from '../../../src/pages/partnerPortal/ppLogin/ppLoginPage';
+import PPLoanDetailsPage from '../../../src/pages/partnerPortal/ppLoanDetails/ppLoanDetailsPage';
 import file from '../../../data/loanpal/application/approved-deferred-stip-data.csv';
 
 const { path, urls } = require('../../../src/utilities/imports');
 
 jest.setTimeout(60000 * 5);
 
-describe('Underwriter Portal Login', () => {
+describe('LP Application', () => {
   let baseTest;
   let lpApp;
-
   beforeAll(async () => {
     baseTest = new BaseTest('chrome');
     lpApp = new LPAppPage(baseTest.webDriver);
   });
 
-  beforeEach(async () => {});
+  afterAll(async () => {
+    if (baseTest) await baseTest.quit();
+  });
 
-  //   afterEach(async () => {
-  //     if (baseTest) await baseTest.close();
-  //   });
-
-  //   afterAll(async () => {
-  //     if (baseTest) await baseTest.quit();
-  //   });
-
-  test.each(file)('Apply for a loan through the loanpal UI', async record => {
-    if (record) {
-      // Get an active loans for the applicant
-      const esClient = new ElasticClient();
-      const activeLoans = await esClient.getActiveLoans(record.ssn);
-      console.log(activeLoans, 'activeLoans');
-      if (process.env.STAGE === 'test' && activeLoans.length > 0) {
-        // Change status to Canceled to avoid DupeKey
-        for (const { id } of activeLoans) {
-          const ld = new LoanData(id);
-          const loan = await ld.getSrcLoan();
-          loan.loanStatus.application = 'Canceled';
-          await ld.putLoan(loan);
-        }
+  let loanID;
+  let stips;
+  test.each(file)(`112714 Apply for a loan through the loanpal UI`, async record => {
+    // Get an active loans for the applicant
+    console.log('CHECKING ACTIVE LOANS');
+    const esClient = new ElasticClient();
+    const activeLoans = await esClient.getActiveLoans(record.ssn);
+    console.log(activeLoans, 'activeLoans');
+    if (process.env.STAGE === 'test' && activeLoans.length > 0) {
+      // Change status to Canceled to avoid DupeKey
+      for (const id of activeLoans) {
+        const ld = new LoanData(id);
+        const loan = await ld.getSrcLoan();
+        loan.loanStatus.application = 'Canceled';
+        await ld.putLoan(loan);
       }
-      // Get Config for provided client
-      const clientData = new ClientData(record.partner);
-      const [clientConfig] = await clientData.getClientConfig();
-      const { loanOptionsMap } = clientConfig;
-      record = { ...record, loanOptionsMap };
-      // console.log(record, 'clientConfig');
-      // Fill out application for provided client url
-      const url = path.join(urls.loanApp, record.url);
-      await lpApp.goToPage(url);
-      await lpApp.fillOutForm(record);
-      await lpApp.reviewInfo();
-      await lpApp.apply();
-      // Assert LoanId is created
-      const loanID = await lpApp.getLoanID();
-      expect(loanID).toEqual(expect.stringMatching(/\d{2}-\d{2}-\d{6}/g));
-      // Assert that the provided stips match the database
-      const newLoan = new LoanData(loanID);
-      const newLoanData = await newLoan.getSrcLoan();
-      const expected = { loanId: loanID, stips: newLoanData.creditDecision.stipulations };
-      const actual = { loanId: loanID, stips: record.stips.split(',').map(item => item.trim()) };
-      expect(expected).toEqual(actual);
     }
+    console.log('CHECKING CLIENT CONFIG');
+    // Get Config for provided client
+    const clientData = new ClientData(record.partner);
+    const [clientConfig] = await clientData.getClientConfig();
+    const { loanOptionsMap } = clientConfig;
+    record = { ...record, loanOptionsMap };
+    // console.log(record, 'clientConfig');
+    // Fill out application for provided client url
+    console.log('FILL OUT APPLICATION');
+    const url = path.join(urls.loanApp, record.url);
+    await lpApp.goToPage(url);
+    await lpApp.fillOutForm(record);
+    await lpApp.reviewInfo();
+    await lpApp.apply();
+    // Assert LoanId is created
+    loanID = await lpApp.getLoanID();
+    expect(loanID).toEqual(expect.stringMatching(/\d{2}-\d{2}-\d{6}/g));
+    // Assert that the provided stips match the database
+    console.log('ASSERT LOAN STIPS');
+    stips = record.stips.split(',').map(item => item.trim());
+    const newLoan = new LoanData(loanID);
+    const newLoanData = await newLoan.getSrcLoan();
+    const expected = { loanId: loanID, stips: newLoanData.creditDecision.stipulations };
+    const actual = { loanId: loanID, stips };
+    expect(expected).toEqual(actual);
+
+    // UW validation
+    console.log('NAVIGATE TO UNDERWRITER');
+    const uwLogin = new UWLoginPage(baseTest.webDriver);
+    await uwLogin.completelogin();
+    const uwLoanDetails = new UWLoanDetailsPage(baseTest.webDriver, loanID);
+    await uwLoanDetails.openURL();
+    // Assert Loan Status
+    const loanStatus = await uwLoanDetails.validateLoanStatus('Approved');
+    expect(loanStatus).toBeTruthy();
+    // Assert Approve Loan Button status
+    const appoveLoanBtn = await uwLoanDetails.validateApproveLoanButton('disabled');
+    expect(appoveLoanBtn).toBeTruthy();
+    // Assert all listed stips
+    for (const stip of stips) {
+      const stipElem = await uwLoanDetails.validateListedStips(stip);
+      expect(stipElem).toBeTruthy();
+    }
+
+    // PP validation
+    console.log('NAVIGATE TO PARTNER PORTAL');
+    const ppLogin = new PPLoginPage(baseTest.webDriver);
+    await ppLogin.completeLogin();
+    const ppLoanDetails = new PPLoanDetailsPage(baseTest.webDriver, loanID);
+    await ppLoanDetails.goToPage();
+    const appStatus = await ppLoanDetails.validateAppStatus('Approved');
+    expect(appStatus).toBeTruthy();
+    const timelineApproval = await ppLoanDetails.validateTimelineApproval();
+    expect(timelineApproval).toBeTruthy();
   });
 });
